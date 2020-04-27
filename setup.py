b935c440a4d4f279773a99d1de73adc4a814753d
earthastronaut <>
2020-04-26 23:13:43 -0600

    add setup.py to make pipfreeze installable

---
+++ setup.py
@@ -0,0 +1,39 @@
+import re
+from setuptools import setup
+
+short_description = "Tool to produce better formatted pip freeze."
+with open('README.md') as f:
+    long_description = f.read()
+
+
+with open('pipfreeze.py') as f:
+    version = next(
+        re.finditer(
+            r'\n__version__ *= *[\'\"]([0-9\.]+)[\'\"]',
+            f.read(),
+        )
+    ).groups()[0]
+
+
+setup(
+    name='pipfreeze',
+    version=version,
+    author='Dylan Gregersen',
+    author_email='an.email0101@gmail.com',
+    url='https://github.com/earthastronaut/storage',
+    license='MIT',
+    description=short_description,
+    long_description=long_description,
+    py_modules=['pipfreeze'],
+    entry_points={
+        'console_scripts': ['pipfreeze = pipfreeze:main']
+    },
+    python_requires='>=2.6',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ]
+)
