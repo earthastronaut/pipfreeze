4287f2bff1ca2aad7a2b434ecc341207a8187162
dylan <dylan@verdigris.co>
2021-02-26 22:30:21 -0700

    make pip 9 capatible by removing Required-by

--- pipfreeze.py
+++ pipfreeze.py
@@ -1,4 +1,4 @@
-#!python
+#!/usr/bin/env python
 """ Better pip freeze which is valid with requirements.txt
 """
 import sys
@@ -26,7 +26,11 @@ __version__ = '1.0.1'
 
 def call_split(cmd):
     """Call command and return output lines."""
-    return subprocess.check_output(cmd).decode("utf-8").rstrip().split("\n")
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+    )
+    process.wait()
+    return process.stdout.read().decode("utf-8").rstrip().split("\n")
 
 
 def name_to_slug(name):
@@ -43,6 +47,26 @@ def split_package_requires(requires):
     ]
 
 
+class Requirement(dict):
+    """ Data for requirements"""
+
+    def __init__(self, name, requirement_version, required_children=None, required_parents=None, **kws):
+        self.name = name
+        self.requirement_version = requirement_version
+        self.required_children = required_children or []
+        self.required_parents = required_parents or []
+        super().__init__(
+            name=self.name,
+            requirement_version=self.requirement_version,
+            required_children=self.required_children,
+            required_parents=self.required_parents,
+            **kws
+        )
+    
+    def __hash__(self):
+        return hash(self.name)
+
+
 def get_package_data(requirement_version):
     """Get data on package.
 
@@ -56,7 +80,9 @@ def get_package_data(requirement_version):
     """
     logger.info("getting package info: %s", requirement_version)
 
-    if "egg=" in requirement_version:
+    if " @ " in requirement_version:
+        name =  requirement_version.split("@")[0]
+    elif "egg=" in requirement_version:
         # parse `-e git+git@github.com:repo/package.git@master#egg=package-name`
         name = requirement_version.split("egg=")[-1]
     else:
@@ -66,13 +92,27 @@ def get_package_data(requirement_version):
     info_lines = call_split(["pip", "show", name])
     data = {}
     for line in info_lines:
+        if ':' not in line:
+            continue
         key, _, value = line.partition(":")
         data[key] = value.strip()
-    data["name"] = name
-    data["requirement_version"] = requirement_version
-    data["required_children"] = split_package_requires(data["Requires"])
-    data["required_parents"] = split_package_requires(data["Required-by"])
-    return data
+    requires = data["Requires"]
+    return Requirement(
+        name=name,
+        requirement_version=requirement_version,
+        required_children=split_package_requires(requires),
+        **data
+    )
+
+def add_parents(requirements_data):
+    """ Get the parents of the requirements """
+    graph_parents = {}
+    for req in requirements_data:
+        for children in req.required_children:
+            graph_parents.setdefault(children, []).append(req.name)
+    for data in requirements_data:
+        data.required_parents = graph_parents.get(data.name, [])
+    return requirements_data
 
 
 def get_requirements(requirements_file=None, processes=None):
@@ -104,14 +144,54 @@ def get_requirements(requirements_file=None, processes=None):
                 get_package_data, requirements_lines, chunksize=5
             ).get()
 
-    return {data["name"]: data for data in results}
+    add_parents(results)
+    return {req.name: req for req in results}
 
 
-def _formatted_requirements_recursive(requirements, recursive_kws=None, **kws):
+def get_formatted_info(info, add_info=None):
+    """ Take the info data and turn into string.
+
+    Parameters:
+        info: dict of information
+        add_info: Keys to use or None to not return any
+    
+    Return:
+        str: formatted string to include in text
+
+    """
+    if add_info is None or add_info == [""]:
+        return ""
+
+    if isinstance(add_info, str):
+        add_info = [add_info]
+    if "all" in add_info:
+        keys = info.keys()
+    else:
+        keys = list(add_info)
+        missing = set(keys) - set(info.keys())
+        if len(missing) > 0:
+            options = "all," + ",".join(info.keys())
+            raise KeyError(
+                "unknown keys in add_info: {} valid options are {}".format(
+                    missing, options
+                )
+            )
+    info_unformatted = {}
+    for key in keys:
+        value = info[key]
+        if value is not None:
+            info_unformatted[key] = value
+    if len(info_unformatted) > 0:
+        info_formatted = "  # info=" + json.dumps(info_unformatted)
+    return info_formatted
+
+
+def _get_formatted_requirements_recursive(requirements, recursive_kws=None, **kws):
     """Recursively format the requirements freeze, nesting dependencies by indenting"""
 
     indent_size = kws["indent_size"]
     add_info = kws["add_info"]
+    exclude = set(kws["exclude"] or [])
 
     recursive_kws = recursive_kws or {}
     names = recursive_kws.get("names")
@@ -121,73 +201,51 @@ def _formatted_requirements_recursive(requirements, recursive_kws=None, **kws):
     if names is None:
         names = [
             key
-            for key, data in requirements.items()
-            if len(data["required_parents"]) == 0  # top level
+            for key, req in requirements.items()
+            if len(req.required_parents) == 0  # top level
         ]
 
     indent = level * indent_size * " "
     lines = []
     for name in sorted(names):
         comment = ""
-        info_options = {
+        info = {
             "required_by": None,
             "included": None,
             "other": None,
         }
 
         if name in DEV_PKGS:
-            info_options["other"] = "dev_pkg"
+            info["other"] = "dev_pkg"
             comment = "# "
-            data = {}
+            req = Requirement(
+                name=name,
+                requirement_version=name,
+            )
+        elif name in exclude:
+            continue
         else:
-            data = requirements[name]
+            req = requirements[name]
             if name in included:
                 comment = "# "
-                info_options["included"] = True
-                info_options["required_by"] = ",".join(data["required_parents"])
-
-        requirement_version = data.get("requirement_version", name)
-        required_children = data.get("required_children", [])
-
-        info_formatted = ""
-        if add_info is not None and add_info != [""]:
-            if isinstance(add_info, str):
-                add_info = [add_info]
-            if "all" in add_info:
-                keys = info_options.keys()
-            else:
-                keys = list(add_info)
-                missing = set(keys) - set(info_options.keys())
-                if len(missing) > 0:
-                    options = "all," + ",".join(info_options.keys())
-                    raise KeyError(
-                        "unknown keys in add_info: {} valid options are {}".format(
-                            missing, options
-                        )
-                    )
-            info_unformatted = {}
-            for key in keys:
-                value = info_options[key]
-                if value is not None:
-                    info_unformatted[key] = value
-            if len(info_unformatted) > 0:
-                info_formatted = "  # info=" + json.dumps(info_unformatted)
+                info["included"] = True
+                info["required_by"] = ",".join(req.required_parents)
 
         lines.append(
             "{indent}{comment}{requirement_version}{info_formatted}".format(
                 indent=indent,
                 comment=comment,
-                requirement_version=requirement_version,
-                info_formatted=info_formatted,
+                requirement_version=req.requirement_version,
+                info_formatted=get_formatted_info(info, add_info=add_info),
             )
         )
         included.add(name)
 
-        if len(required_children) > 0:
-            children_included, children_lines = _formatted_requirements_recursive(
+        if len(req.required_children) > 0:
+            children_included, children_lines = _get_formatted_requirements_recursive(
                 requirements,
                 recursive_kws=dict(
-                    names=required_children,
+                    names=req.required_children,
                     level=level + 1,
                     included=included,
                 ),
@@ -199,7 +257,7 @@ def _formatted_requirements_recursive(requirements, recursive_kws=None, **kws):
     return included, lines
 
 
-def formatted_requirements(requirements, indent_size=4, add_info=None):
+def formatted_requirements(requirements, indent_size=4, add_info=None, exclude=None):
     """Format requirements into new nested format.
 
     Parameters:
@@ -211,8 +269,8 @@ def formatted_requirements(requirements, indent_size=4, add_info=None):
         str: New joined lines.
 
     """
-    _, lines = _formatted_requirements_recursive(
-        requirements, indent_size=indent_size, add_info=add_info
+    _, lines = _get_formatted_requirements_recursive(
+        requirements, indent_size=indent_size, add_info=add_info, exclude=exclude,
     )
     return "\n".join(lines)
 
@@ -257,6 +315,11 @@ def cli():
         ),
     )
     parser.add_argument(
+        "--exclude",
+        default="",
+        help="Exclude packages. Comma separated.",
+    )    
+    parser.add_argument(
         "--indent-size",
         type=int,
         default=4,
@@ -280,12 +343,19 @@ def cli():
     if loglevel:
         logging.basicConfig(level=loglevel.upper())
 
+    exclude = [
+        name_to_slug(name) for name in pargs.exclude.split(",")
+    ]
+
     requirements = get_requirements(
         requirements_file=pargs.requirements_file,
         processes=pargs.processes,
     )
     content = formatted_requirements(
-        requirements, indent_size=pargs.indent_size, add_info=pargs.add_info.split(",")
+        requirements,
+        indent_size=pargs.indent_size,
+        add_info=pargs.add_info.split(","),
+        exclude=exclude,
     )
     if pargs.output is None:
         logger.info("---- pip freeze ----")
@@ -300,6 +370,6 @@ def cli():
 
 
 if __name__ == "__main__":
-    reqs = get_requirements()
+    reqs = get_requirements(processes=1)
     content = formatted_requirements(reqs)
     print(content)
